9677ff7d97ae44e37230152794f56be9d52c7be5
Viacheslav Terentyev <terentyev.va@yandex.ru>
2022-07-09 08:17:41 +0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Вячеслав Терентьев on 09.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
